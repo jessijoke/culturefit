b84e6ca7c127b4f368dff51ae9a016ef3ad47501
jessijoke <jessijoke@yahoo.com>
2021-10-30 03:51:17 -0400

    Fixing small login bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,15 @@
 class UsersController < ApplicationController
-    #skip_before_action :authorized, only: [:create, :login]
-    #before_action :authorized, only: [:auto_login]
+    skip_before_action :authorized, only: [:create, :login]
+    before_action :authorized, only: [:auto_login]
 
     def index
         users = User.all
-        #render json: UserSerializer.new(users)
         render json: users
     end
 
     def show
         user = User.find_by(id: params[:id])
         if (user)
-            #render json: UserSerializer.new(user)
             render json: user
         else
             render json: { message: 'User not found.'}
@@ -19,10 +17,9 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
+        @user = User.create(name: user_params[:name], password: user_params[:password], email: user_params[:email], user_type: user_params[:user_type])
         if @user.valid?
           @token = encode_token(user_id: @user.id)
-          #render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
           render json: { user: @user, jwt: @token }
         else
           render json: { error: 'failed to create user' }, status: :not_acceptable
--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -4,5 +4,4 @@ class QuizSerializer < ActiveModel::Serializer
     #attributes :quiz_name, :user_id, :get_answers
     has_many :quiz_questions
     has_many :user_attributes
-    
 end
