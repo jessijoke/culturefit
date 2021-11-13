385fc1912adee7fd9aacf6a17655e82cd1eae904
jessijoke <jessijoke@yahoo.com>
2021-11-12 21:22:10 -0500

    Deploy attemp

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    #origins 'https://cors-anywhere.herokuapp.com/https://jessijoke.github.io/culturefit-front/'
-    origins '*'
+    origins 'http://jessijoke.github.io/culturefit-front/'
+    #origins '*'
     ##For testing ^^
 
     resource '*',
