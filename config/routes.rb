aa4597b9b95210f1ba8a310104fe4ca012cd4c83
jessijoke <jessijoke@yahoo.com>
2021-11-17 01:51:58 -0500

    Testing slight route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :messages
   get 'private/test'
-  get '/current_user', to: 'current_user#index'
-  get '/user_attributes/unique', to: 'user_attributes#unique_elements'
   resources :answer_attributes
   resources :quiz_answers
   resources :quiz_questions
@@ -10,5 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :user_attributes
   post "/login", to: "users#login"
+  get '/current_user', to: 'current_user#index'
+  get '/user_attributes/unique', to: 'user_attributes#unique_elements'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
