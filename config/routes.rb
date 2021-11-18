4f7438d9feb5c2b561344f6564ecfe64f7507394
jessijoke <jessijoke@yahoo.com>
2021-11-17 23:26:51 -0500

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'private/test'
+  post "/login", to: "users#login"
+  get 'current_user', to: 'current_user#index'
+  get 'user_attributes/unique' => 'user_attributes#unique'
   resources :messages
   resources :answer_attributes
   resources :quiz_answers
@@ -6,9 +10,6 @@ Rails.application.routes.draw do
   resources :quizzes
   resources :users
   resources :user_attributes
-  get 'private/test'
-  post "/login", to: "users#login"
-  get 'current_user', to: 'current_user#index'
-  get 'user_attributes/unique' => 'user_attribute#unique'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
