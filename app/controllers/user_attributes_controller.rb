f8325de9af0b92adc08b1be7063c0a4583e6a82d
jessijoke <jessijoke@yahoo.com>
2021-11-17 23:49:41 -0500

    Test

--- app/controllers/user_attributes_controller.rb
+++ app/controllers/user_attributes_controller.rb
@@ -8,8 +8,8 @@ class UserAttributesController < ApplicationController
     def unique
         #scores = UserAttribute.group(:quiz_id)
         #scores = UserAttribute.select(:quiz_id).distinct
-        #scores = UserAttribute.all
-        scores = UserAttribute.select(:quiz_id)
+        scores = UserAttribute.all
+        #scores = UserAttribute.select(:quiz_id)
         render json: scores
         #render json: { message: "Something went wrong." }
     end
