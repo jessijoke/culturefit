62faadfdc8831c5bff4480ae0807c3b689b722ae
jessijoke <jessijoke@yahoo.com>
2021-11-17 23:47:34 -0500

    Testing again

--- app/serializers/user_attribute_serializer.rb
+++ app/serializers/user_attribute_serializer.rb
@@ -1,5 +1,5 @@
 class UserAttributeSerializer < ActiveModel::Serializer
-  attributes :id, :score, :attr, :quiz, :quiz_id
+  attributes :id, :score, :attr, :quiz, :quiz_id, :user_id
   belongs_to :user
   belongs_to :quiz
 end
