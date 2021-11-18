f034fd071121f096b6889f1ecc06d9bd868c39eb
jessijoke <jessijoke@yahoo.com>
2021-11-17 22:05:41 -0500

    was it the serializer? fr?

--- app/serializers/user_attribute_serializer.rb
+++ app/serializers/user_attribute_serializer.rb
@@ -1,5 +1,5 @@
 class UserAttributeSerializer < ActiveModel::Serializer
-  attributes :id, :score, :attr, :quiz, :quiz_id
+  attributes :id, :score, :attr, :quiz, :quiz_id, :message
   belongs_to :user
   belongs_to :quiz
 end
